b4a5baf281fe21bda99c62bc054f67c7564c4442
kai618 <vanhieu1601@gmail.com>
2020-05-06 16:42:22 +0700

    added some info for note

--- test/https-post/https-post.ino
+++ test/https-post/https-post.ino
@@ -4,25 +4,24 @@
 String ssid = "GHC_";
 String password = "12356789";
 
-String host = "api.github.com";
-const int port = 443;
-
-// SHA1 fingerprint of the certificate
-String fingerprint = "59 74 61 88 13 CA 12 34 15 4D 11 0A C1 7F E6 67 07 69 42 F5";
-
 void setup() {
   Serial.begin(115200);
+  Serial.setDebugOutput(true);
   
   connectWifi(ssid, password);
 
   WiFiClientSecure client;  
+  const int port = 443;
+  String host = "api.github.com";
+  String url = "/zen";
+  String fingerprint = "59 74 61 88 13 CA 12 34 15 4D 11 0A C1 7F E6 67 07 69 42 F5";
   checkFingerprintMatch(client, host, port, fingerprint);
- 
-  String url = "/zen";  
-//  Serial.println(sendHttpsGet(client, host, port, url));
+  Serial.println(sendHttpsGet(client, host, port, url));
 
   String host2 = "heroku-nodejs-api-test.herokuapp.com";
   String url2 = "/arduino";
+  String fingerprint2 = "08 3B 71 72 02 43 6E CA ED 42 86 93 BA 7E DF 81 C4 BC 62 30";
+  checkFingerprintMatch(client, host2, port, fingerprint2);
   Serial.println(sendHttpsPost(client, host2, port, url2, "Kai"));
 }
 
@@ -31,18 +30,16 @@ void loop() {
 }
 
 String sendHttpsGet(WiFiClientSecure httpsClient, String host, int port, String url) {  
-  if (httpsClient.connect(host.c_str(), port)) {   
-    Serial.println("--Sent GET request to " + host); 
-    
+  if (httpsClient.connect(host.c_str(), port)) {         
     httpsClient.println("GET " + url + " HTTP/1.1");
     httpsClient.println("Host: " + host);
     httpsClient.println("User-Agent: ESP8266/Hieu");
     httpsClient.println("Connection: close\r\n");    
-    
+    Serial.println("--Sent GET request to " + host);
+
     String response = httpsClient.readString();
     int bodypos = response.indexOf("\r\n\r\n") + 4;
-    
-    Serial.println("--Get request ended successfully");
+    Serial.println("--GET request ended successfully");
     return response.substring(bodypos);
   }
   else {
@@ -52,9 +49,7 @@ String sendHttpsGet(WiFiClientSecure httpsClient, String host, int port, String 
 }
 
 String sendHttpsPost(WiFiClientSecure httpsClient, String host, int port, String url, String data) {
-  if (httpsClient.connect(host, port)) {
-    Serial.println("--Sent POST request to " + host); 
-    
+  if (httpsClient.connect(host, port)) {    
     httpsClient.println("POST " + url + " HTTP/1.1");
     httpsClient.println("Host: " + host);
     httpsClient.println("User-Agent: ESP8266/Hieu");
@@ -65,6 +60,7 @@ String sendHttpsPost(WiFiClientSecure httpsClient, String host, int port, String
     httpsClient.println();
     httpsClient.println(data);
     delay(10);
+    Serial.println("--Sent POST request to " + host);
     
     String response = httpsClient.readString();
     int bodypos =  response.indexOf("\r\n\r\n") + 4;
